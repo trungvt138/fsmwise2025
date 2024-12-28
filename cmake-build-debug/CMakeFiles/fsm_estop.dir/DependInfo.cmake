070a23273c3597041f40eea120f8d6dbfba39d13
Đàm Quang Trung <trungvt138@github.com>
2024-12-28 17:42:49 +0100

    operatingfsm2 uploading

---
+++ cmake-build-debug/CMakeFiles/fsm_estop.dir/DependInfo.cmake
@@ -0,0 +1,78 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "C:/Users/trung/CLionProjects/fsmwise2025/calibratingfsm/calibratingbasestate.cpp" "CMakeFiles/fsm_estop.dir/calibratingfsm/calibratingbasestate.cpp.obj" "gcc" "CMakeFiles/fsm_estop.dir/calibratingfsm/calibratingbasestate.cpp.obj.d"
+  "C:/Users/trung/CLionProjects/fsmwise2025/estopfsm/basestateestop.cpp" "CMakeFiles/fsm_estop.dir/estopfsm/basestateestop.cpp.obj" "gcc" "CMakeFiles/fsm_estop.dir/estopfsm/basestateestop.cpp.obj.d"
+  "C:/Users/trung/CLionProjects/fsmwise2025/estopfsm/bothpressed.cpp" "CMakeFiles/fsm_estop.dir/estopfsm/bothpressed.cpp.obj" "gcc" "CMakeFiles/fsm_estop.dir/estopfsm/bothpressed.cpp.obj.d"
+  "C:/Users/trung/CLionProjects/fsmwise2025/estopfsm/bothpressedcl.cpp" "CMakeFiles/fsm_estop.dir/estopfsm/bothpressedcl.cpp.obj" "gcc" "CMakeFiles/fsm_estop.dir/estopfsm/bothpressedcl.cpp.obj.d"
+  "C:/Users/trung/CLionProjects/fsmwise2025/estopfsm/connectionlost.cpp" "CMakeFiles/fsm_estop.dir/estopfsm/connectionlost.cpp.obj" "gcc" "CMakeFiles/fsm_estop.dir/estopfsm/connectionlost.cpp.obj.d"
+  "C:/Users/trung/CLionProjects/fsmwise2025/estopfsm/e1pressed.cpp" "CMakeFiles/fsm_estop.dir/estopfsm/e1pressed.cpp.obj" "gcc" "CMakeFiles/fsm_estop.dir/estopfsm/e1pressed.cpp.obj.d"
+  "C:/Users/trung/CLionProjects/fsmwise2025/estopfsm/e1pressedcl.cpp" "CMakeFiles/fsm_estop.dir/estopfsm/e1pressedcl.cpp.obj" "gcc" "CMakeFiles/fsm_estop.dir/estopfsm/e1pressedcl.cpp.obj.d"
+  "C:/Users/trung/CLionProjects/fsmwise2025/estopfsm/e2pressed.cpp" "CMakeFiles/fsm_estop.dir/estopfsm/e2pressed.cpp.obj" "gcc" "CMakeFiles/fsm_estop.dir/estopfsm/e2pressed.cpp.obj.d"
+  "C:/Users/trung/CLionProjects/fsmwise2025/estopfsm/resetFSM.cpp" "CMakeFiles/fsm_estop.dir/estopfsm/resetFSM.cpp.obj" "gcc" "CMakeFiles/fsm_estop.dir/estopfsm/resetFSM.cpp.obj.d"
+  "C:/Users/trung/CLionProjects/fsmwise2025/fsm/basestate.cpp" "CMakeFiles/fsm_estop.dir/fsm/basestate.cpp.obj" "gcc" "CMakeFiles/fsm_estop.dir/fsm/basestate.cpp.obj.d"
+  "C:/Users/trung/CLionProjects/fsmwise2025/fsm/estopFSM.cpp" "CMakeFiles/fsm_estop.dir/fsm/estopFSM.cpp.obj" "gcc" "CMakeFiles/fsm_estop.dir/fsm/estopFSM.cpp.obj.d"
+  "C:/Users/trung/CLionProjects/fsmwise2025/fsm/mainFSM.cpp" "CMakeFiles/fsm_estop.dir/fsm/mainFSM.cpp.obj" "gcc" "CMakeFiles/fsm_estop.dir/fsm/mainFSM.cpp.obj.d"
+  "C:/Users/trung/CLionProjects/fsmwise2025/fsm/pseudostartstate.cpp" "CMakeFiles/fsm_estop.dir/fsm/pseudostartstate.cpp.obj" "gcc" "CMakeFiles/fsm_estop.dir/fsm/pseudostartstate.cpp.obj.d"
+  "C:/Users/trung/CLionProjects/fsmwise2025/main.cpp" "CMakeFiles/fsm_estop.dir/main.cpp.obj" "gcc" "CMakeFiles/fsm_estop.dir/main.cpp.obj.d"
+  "C:/Users/trung/CLionProjects/fsmwise2025/mainfsm/basestatemain.cpp" "CMakeFiles/fsm_estop.dir/mainfsm/basestatemain.cpp.obj" "gcc" "CMakeFiles/fsm_estop.dir/mainfsm/basestatemain.cpp.obj.d"
+  "C:/Users/trung/CLionProjects/fsmwise2025/mainfsm/calibrating.cpp" "CMakeFiles/fsm_estop.dir/mainfsm/calibrating.cpp.obj" "gcc" "CMakeFiles/fsm_estop.dir/mainfsm/calibrating.cpp.obj.d"
+  "C:/Users/trung/CLionProjects/fsmwise2025/mainfsm/idle.cpp" "CMakeFiles/fsm_estop.dir/mainfsm/idle.cpp.obj" "gcc" "CMakeFiles/fsm_estop.dir/mainfsm/idle.cpp.obj.d"
+  "C:/Users/trung/CLionProjects/fsmwise2025/mainfsm/operating.cpp" "CMakeFiles/fsm_estop.dir/mainfsm/operating.cpp.obj" "gcc" "CMakeFiles/fsm_estop.dir/mainfsm/operating.cpp.obj.d"
+  "C:/Users/trung/CLionProjects/fsmwise2025/misc/action.cpp" "CMakeFiles/fsm_estop.dir/misc/action.cpp.obj" "gcc" "CMakeFiles/fsm_estop.dir/misc/action.cpp.obj.d"
+  "C:/Users/trung/CLionProjects/fsmwise2025/misc/context.cpp" "CMakeFiles/fsm_estop.dir/misc/context.cpp.obj" "gcc" "CMakeFiles/fsm_estop.dir/misc/context.cpp.obj.d"
+  "C:/Users/trung/CLionProjects/fsmwise2025/misc/contextdata.cpp" "CMakeFiles/fsm_estop.dir/misc/contextdata.cpp.obj" "gcc" "CMakeFiles/fsm_estop.dir/misc/contextdata.cpp.obj.d"
+  "C:/Users/trung/CLionProjects/fsmwise2025/operatingfsm1/err_lost_ws.cpp" "CMakeFiles/fsm_estop.dir/operatingfsm1/err_lost_ws.cpp.obj" "gcc" "CMakeFiles/fsm_estop.dir/operatingfsm1/err_lost_ws.cpp.obj.d"
+  "C:/Users/trung/CLionProjects/fsmwise2025/operatingfsm1/err_new_ws.cpp" "CMakeFiles/fsm_estop.dir/operatingfsm1/err_new_ws.cpp.obj" "gcc" "CMakeFiles/fsm_estop.dir/operatingfsm1/err_new_ws.cpp.obj.d"
+  "C:/Users/trung/CLionProjects/fsmwise2025/operatingfsm1/err_slide_full.cpp" "CMakeFiles/fsm_estop.dir/operatingfsm1/err_slide_full.cpp.obj" "gcc" "CMakeFiles/fsm_estop.dir/operatingfsm1/err_slide_full.cpp.obj.d"
+  "C:/Users/trung/CLionProjects/fsmwise2025/operatingfsm1/error.cpp" "CMakeFiles/fsm_estop.dir/operatingfsm1/error.cpp.obj" "gcc" "CMakeFiles/fsm_estop.dir/operatingfsm1/error.cpp.obj.d"
+  "C:/Users/trung/CLionProjects/fsmwise2025/operatingfsm1/errorfsm/errorbasestate.cpp" "CMakeFiles/fsm_estop.dir/operatingfsm1/errorfsm/errorbasestate.cpp.obj" "gcc" "CMakeFiles/fsm_estop.dir/operatingfsm1/errorfsm/errorbasestate.cpp.obj.d"
+  "C:/Users/trung/CLionProjects/fsmwise2025/operatingfsm1/errorfsm/handlingerror.cpp" "CMakeFiles/fsm_estop.dir/operatingfsm1/errorfsm/handlingerror.cpp.obj" "gcc" "CMakeFiles/fsm_estop.dir/operatingfsm1/errorfsm/handlingerror.cpp.obj.d"
+  "C:/Users/trung/CLionProjects/fsmwise2025/operatingfsm1/errorfsm/waiterror.cpp" "CMakeFiles/fsm_estop.dir/operatingfsm1/errorfsm/waiterror.cpp.obj" "gcc" "CMakeFiles/fsm_estop.dir/operatingfsm1/errorfsm/waiterror.cpp.obj.d"
+  "C:/Users/trung/CLionProjects/fsmwise2025/operatingfsm1/fb1.cpp" "CMakeFiles/fsm_estop.dir/operatingfsm1/fb1.cpp.obj" "gcc" "CMakeFiles/fsm_estop.dir/operatingfsm1/fb1.cpp.obj.d"
+  "C:/Users/trung/CLionProjects/fsmwise2025/operatingfsm1/fb1runfsm/fb1runbasestate.cpp" "CMakeFiles/fsm_estop.dir/operatingfsm1/fb1runfsm/fb1runbasestate.cpp.obj" "gcc" "CMakeFiles/fsm_estop.dir/operatingfsm1/fb1runfsm/fb1runbasestate.cpp.obj.d"
+  "C:/Users/trung/CLionProjects/fsmwise2025/operatingfsm1/fb1runfsm/fb1runidle.cpp" "CMakeFiles/fsm_estop.dir/operatingfsm1/fb1runfsm/fb1runidle.cpp.obj" "gcc" "CMakeFiles/fsm_estop.dir/operatingfsm1/fb1runfsm/fb1runidle.cpp.obj.d"
+  "C:/Users/trung/CLionProjects/fsmwise2025/operatingfsm1/fb1runfsm/fb1running.cpp" "CMakeFiles/fsm_estop.dir/operatingfsm1/fb1runfsm/fb1running.cpp.obj" "gcc" "CMakeFiles/fsm_estop.dir/operatingfsm1/fb1runfsm/fb1running.cpp.obj.d"
+  "C:/Users/trung/CLionProjects/fsmwise2025/operatingfsm1/fb1runfsm/fb1runwait.cpp" "CMakeFiles/fsm_estop.dir/operatingfsm1/fb1runfsm/fb1runwait.cpp.obj" "gcc" "CMakeFiles/fsm_estop.dir/operatingfsm1/fb1runfsm/fb1runwait.cpp.obj.d"
+  "C:/Users/trung/CLionProjects/fsmwise2025/operatingfsm1/fb1sortfsm/fb1sortbasestate.cpp" "CMakeFiles/fsm_estop.dir/operatingfsm1/fb1sortfsm/fb1sortbasestate.cpp.obj" "gcc" "CMakeFiles/fsm_estop.dir/operatingfsm1/fb1sortfsm/fb1sortbasestate.cpp.obj.d"
+  "C:/Users/trung/CLionProjects/fsmwise2025/operatingfsm1/fb1sortfsm/fb1sortok.cpp" "CMakeFiles/fsm_estop.dir/operatingfsm1/fb1sortfsm/fb1sortok.cpp.obj" "gcc" "CMakeFiles/fsm_estop.dir/operatingfsm1/fb1sortfsm/fb1sortok.cpp.obj.d"
+  "C:/Users/trung/CLionProjects/fsmwise2025/operatingfsm1/fb1sortfsm/fb1sortout.cpp" "CMakeFiles/fsm_estop.dir/operatingfsm1/fb1sortfsm/fb1sortout.cpp.obj" "gcc" "CMakeFiles/fsm_estop.dir/operatingfsm1/fb1sortfsm/fb1sortout.cpp.obj.d"
+  "C:/Users/trung/CLionProjects/fsmwise2025/operatingfsm1/fb1sortfsm/fb1warning.cpp" "CMakeFiles/fsm_estop.dir/operatingfsm1/fb1sortfsm/fb1warning.cpp.obj" "gcc" "CMakeFiles/fsm_estop.dir/operatingfsm1/fb1sortfsm/fb1warning.cpp.obj.d"
+  "C:/Users/trung/CLionProjects/fsmwise2025/operatingfsm1/operatingbasestate1.cpp" "CMakeFiles/fsm_estop.dir/operatingfsm1/operatingbasestate1.cpp.obj" "gcc" "CMakeFiles/fsm_estop.dir/operatingfsm1/operatingbasestate1.cpp.obj.d"
+  "C:/Users/trung/CLionProjects/fsmwise2025/operatingfsm1/runningfsm/fast.cpp" "CMakeFiles/fsm_estop.dir/operatingfsm1/runningfsm/fast.cpp.obj" "gcc" "CMakeFiles/fsm_estop.dir/operatingfsm1/runningfsm/fast.cpp.obj.d"
+  "C:/Users/trung/CLionProjects/fsmwise2025/operatingfsm1/runningfsm/runningbasestate.cpp" "CMakeFiles/fsm_estop.dir/operatingfsm1/runningfsm/runningbasestate.cpp.obj" "gcc" "CMakeFiles/fsm_estop.dir/operatingfsm1/runningfsm/runningbasestate.cpp.obj.d"
+  "C:/Users/trung/CLionProjects/fsmwise2025/operatingfsm1/runningfsm/slow.cpp" "CMakeFiles/fsm_estop.dir/operatingfsm1/runningfsm/slow.cpp.obj" "gcc" "CMakeFiles/fsm_estop.dir/operatingfsm1/runningfsm/slow.cpp.obj.d"
+  "C:/Users/trung/CLionProjects/fsmwise2025/operatingfsm1/slowfsm/basestateslow1.cpp" "CMakeFiles/fsm_estop.dir/operatingfsm1/slowfsm/basestateslow1.cpp.obj" "gcc" "CMakeFiles/fsm_estop.dir/operatingfsm1/slowfsm/basestateslow1.cpp.obj.d"
+  "C:/Users/trung/CLionProjects/fsmwise2025/operatingfsm1/slowfsm/bore1.cpp" "CMakeFiles/fsm_estop.dir/operatingfsm1/slowfsm/bore1.cpp.obj" "gcc" "CMakeFiles/fsm_estop.dir/operatingfsm1/slowfsm/bore1.cpp.obj.d"
+  "C:/Users/trung/CLionProjects/fsmwise2025/operatingfsm1/slowfsm/flat1.cpp" "CMakeFiles/fsm_estop.dir/operatingfsm1/slowfsm/flat1.cpp.obj" "gcc" "CMakeFiles/fsm_estop.dir/operatingfsm1/slowfsm/flat1.cpp.obj.d"
+  "C:/Users/trung/CLionProjects/fsmwise2025/operatingfsm1/slowfsm/high1.cpp" "CMakeFiles/fsm_estop.dir/operatingfsm1/slowfsm/high1.cpp.obj" "gcc" "CMakeFiles/fsm_estop.dir/operatingfsm1/slowfsm/high1.cpp.obj.d"
+  "C:/Users/trung/CLionProjects/fsmwise2025/operatingfsm1/slowfsm/idleslow1.cpp" "CMakeFiles/fsm_estop.dir/operatingfsm1/slowfsm/idleslow1.cpp.obj" "gcc" "CMakeFiles/fsm_estop.dir/operatingfsm1/slowfsm/idleslow1.cpp.obj.d"
+  "C:/Users/trung/CLionProjects/fsmwise2025/operatingfsm1/slowfsm/ws_bin1.cpp" "CMakeFiles/fsm_estop.dir/operatingfsm1/slowfsm/ws_bin1.cpp.obj" "gcc" "CMakeFiles/fsm_estop.dir/operatingfsm1/slowfsm/ws_bin1.cpp.obj.d"
+  "C:/Users/trung/CLionProjects/fsmwise2025/operatingfsm1/slowfsm/ws_bore1.cpp" "CMakeFiles/fsm_estop.dir/operatingfsm1/slowfsm/ws_bore1.cpp.obj" "gcc" "CMakeFiles/fsm_estop.dir/operatingfsm1/slowfsm/ws_bore1.cpp.obj.d"
+  "C:/Users/trung/CLionProjects/fsmwise2025/operatingfsm1/slowfsm/ws_flat1.cpp" "CMakeFiles/fsm_estop.dir/operatingfsm1/slowfsm/ws_flat1.cpp.obj" "gcc" "CMakeFiles/fsm_estop.dir/operatingfsm1/slowfsm/ws_flat1.cpp.obj.d"
+  "C:/Users/trung/CLionProjects/fsmwise2025/operatingfsm1/slowfsm/ws_high1.cpp" "CMakeFiles/fsm_estop.dir/operatingfsm1/slowfsm/ws_high1.cpp.obj" "gcc" "CMakeFiles/fsm_estop.dir/operatingfsm1/slowfsm/ws_high1.cpp.obj.d"
+  "C:/Users/trung/CLionProjects/fsmwise2025/operatingfsm1/warn_sort_not_possible.cpp" "CMakeFiles/fsm_estop.dir/operatingfsm1/warn_sort_not_possible.cpp.obj" "gcc" "CMakeFiles/fsm_estop.dir/operatingfsm1/warn_sort_not_possible.cpp.obj.d"
+  "C:/Users/trung/CLionProjects/fsmwise2025/operatingfsm2/operatingbasestate2.cpp" "CMakeFiles/fsm_estop.dir/operatingfsm2/operatingbasestate2.cpp.obj" "gcc" "CMakeFiles/fsm_estop.dir/operatingfsm2/operatingbasestate2.cpp.obj.d"
+  "C:/Users/trung/CLionProjects/fsmwise2025/resetfsm/basestatereset.cpp" "CMakeFiles/fsm_estop.dir/resetfsm/basestatereset.cpp.obj" "gcc" "CMakeFiles/fsm_estop.dir/resetfsm/basestatereset.cpp.obj.d"
+  "C:/Users/trung/CLionProjects/fsmwise2025/resetfsm/r1pressed.cpp" "CMakeFiles/fsm_estop.dir/resetfsm/r1pressed.cpp.obj" "gcc" "CMakeFiles/fsm_estop.dir/resetfsm/r1pressed.cpp.obj.d"
+  "C:/Users/trung/CLionProjects/fsmwise2025/resetfsm/r2pressed.cpp" "CMakeFiles/fsm_estop.dir/resetfsm/r2pressed.cpp.obj" "gcc" "CMakeFiles/fsm_estop.dir/resetfsm/r2pressed.cpp.obj.d"
+  "C:/Users/trung/CLionProjects/fsmwise2025/resetfsm/wait.cpp" "CMakeFiles/fsm_estop.dir/resetfsm/wait.cpp.obj" "gcc" "CMakeFiles/fsm_estop.dir/resetfsm/wait.cpp.obj.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
